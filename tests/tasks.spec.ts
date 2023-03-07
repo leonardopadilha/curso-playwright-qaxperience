04f791be770515dc2d05bf9237eb9af387714a5c
leonardopadilha <leonardopadilha28@gmail.com>
2023-03-07 05:20:50 -0300

    Testes independentes

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -63,3 +63,25 @@ test('Deve poder cadastrar uma nova tarefa utilizando recurso exclusivo do playw
     await expect(textoEsperado).toBeVisible();
 });
 
+test('não deve permitir tarefa duplicada', async ({ page, request }) => {
+
+    const task = {
+        name: 'Comprar ketchup',
+        is_done: false
+    }
+
+    await request.delete('http://localhost:3333/helper/tasks/' + task.name);
+    
+    const newTask = await request.post('http://localhost:3333/tasks', {data: task});
+    expect(newTask.ok()).toBeTruthy()
+
+    await page.goto('http://localhost:3000')
+
+    const inputTaskName = page.locator('input[placeholder="Add a new Task"]')
+    await inputTaskName.fill(task.name)
+
+    await page.click('css=button >> text=Create')
+
+    const target = page.locator('.swal2-html-container')
+    await expect(target).toHaveText('Task already exists!')
+});
