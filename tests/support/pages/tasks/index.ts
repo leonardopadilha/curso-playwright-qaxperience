0618fbf57bad227212a2ff6b1844358f91c75656
leonardopadilha <leonardopadilha28@gmail.com>
2023-03-12 09:56:45 -0300

    qqqqqq

--- tests/support/pages/tasks/index.ts
+++ tests/support/pages/tasks/index.ts
@@ -16,6 +16,13 @@ export class TasksPage {
         await this.page.click('button[class*="ButtonNewTask"]')
     }
 
+    async toogle(taskName: string) {
+        const localizador = `xpath=//p[text()="${taskName}"]/..//button[contains(@class, "Toggle")]`;
+        
+        const target = this.page.locator(localizador)
+        await target.click()
+    }
+
     async go() {
         await this.page.goto('http://localhost:3000')
     }
@@ -28,5 +35,10 @@ export class TasksPage {
     async alertHaveText(text: string) {
         const target = this.page.locator('.swal2-html-container')
         await expect(target).toHaveText(text)
+    }
+
+    async shouldBeDone(tasName: string) {
+        const target = this.page.getByText(tasName)
+        await expect(target).toHaveCSS('text-decoration-line', 'line-through')
     }
 }
--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -6,11 +6,12 @@ import { TasksPage } from "./support/pages/tasks";
 import data from "./fixtures/tasks.json";
 //import { faker } from '@faker-js/faker';
 
-test('Deve poder cadastrar uma nova tarefa utilizando CSS Selector', async ({ page, request }) => {
+test.describe("cadastro", () => {
+  test("Deve poder cadastrar uma nova tarefa utilizando CSS Selector", async ({ page, request }) => {
     const task = data.success as TaskModel;
 
     await deleteTaskByHelper(request, task.name);
-    
+
     //const textoDigitado = faker.lorem.words();
 
     const tasksPage: TasksPage = new TasksPage(page);
@@ -18,80 +19,107 @@ test('Deve poder cadastrar uma nova tarefa utilizando CSS Selector', async ({ pa
     await tasksPage.go();
     await tasksPage.create(task);
     await tasksPage.shouldHaveText(task.name);
-    
+
     //let textoEsperado = page.locator('div:nth-child(3) > p');
-    
-});
+  });
 
-/* test.skip('Deve poder cadastrar uma nova tarefa utilizando Xpath', async ({ page, request }) => {
+  test.skip("Deve poder cadastrar uma nova tarefa utilizando Xpath", async ({
+    page,
+    request,
+  }) => {
+    const texto_digitado = "Ler livro de JavaScript";
 
-    const texto_digitado = 'Ler livro de JavaScript';
-
-    await request.delete(`http://localhost:3333/helper/tasks/${texto_digitado}`)
+    await request.delete(
+      `http://localhost:3333/helper/tasks/${texto_digitado}`
+    );
 
     const tasksPage: TasksPage = new TasksPage(page);
     tasksPage.go();
-    
+
     //const texto_digitado = faker.lorem.words();
 
     await inputTaskName.fill(texto_digitado);
 
-    const buttonSubmit = page.locator('xpath=//button[contains(text(),"Create")]');
+    const buttonSubmit = page.locator(
+      'xpath=//button[contains(text(),"Create")]'
+    );
     await buttonSubmit.click();
 
     //let textoEsperado = page.locator('div:nth-child(3) > p');
-    let textoEsperado = page.locator(`css=.task-item p >> text=${texto_digitado}`);
+    let textoEsperado = page.locator(
+      `css=.task-item p >> text=${texto_digitado}`
+    );
     await expect(textoEsperado).toContainText(texto_digitado);
-}); */
+  });
 
-/* test.skip('Deve poder cadastrar uma nova tarefa utilizando recurso exclusivo do playwright', async ({ page, request }) => {
+  test.skip("Deve poder cadastrar uma nova tarefa utilizando recurso exclusivo do playwright", async ({
+    page,
+    request,
+  }) => {
+    const textoDigitado = "Finalizar curso de playwright - QAx";
 
-    const textoDigitado = 'Finalizar curso de playwright - QAx';
-
-    await request.delete(`http://localhost:3333/helper/tasks/${textoDigitado}`)
-
+    await request.delete(`http://localhost:3333/helper/tasks/${textoDigitado}`);
 
     const tasksPage: TasksPage = new TasksPage(page);
     tasksPage.go();
-  
+
     //const textoDigitado = faker.lorem.words();
 
-    const inputTaskName = page.locator('input[placeholder="Add a new Task"]')
+    const inputTaskName = page.locator('input[placeholder="Add a new Task"]');
     await inputTaskName.fill(textoDigitado);
 
-    await page.click('css=button >> text=Create')
+    await page.click("css=button >> text=Create");
 
     //let textoEsperado = page.locator('div:nth-child(3) > p');
     //Combinando css com estratégia de localização por texto
-    let textoEsperado = page.locator(`css=.task-item p >> text=${textoDigitado}`);
+    let textoEsperado = page.locator(
+      `css=.task-item p >> text=${textoDigitado}`
+    );
     await expect(textoEsperado).toBeVisible();
-}); */
+  });
 
-test('não deve permitir tarefa duplicada', async ({ page, request }) => {
+  test("não deve permitir tarefa duplicada", async ({ page, request }) => {
     const task = data.duplicate as TaskModel;
 
     await deleteTaskByHelper(request, task.name);
-    await postTask(request, task)
+    await postTask(request, task);
 
     const tasksPage: TasksPage = new TasksPage(page);
 
     await tasksPage.go();
     await tasksPage.create(task);
-    await tasksPage.alertHaveText('Task already exists!');
- 
+    await tasksPage.alertHaveText("Task already exists!");
+
     //await page.click('css=button >> text=Create')
-});
+  });
 
-test('campo obrigatório', async ({ page }) => {
-
+  test("campo obrigatório", async ({ page }) => {
     const task = data.required as TaskModel;
-    
+
     const taskPage: TasksPage = new TasksPage(page);
 
     await taskPage.go();
     await taskPage.create(task);
 
     //campo obrigatório sem a mensagem em HTML
-    const validationMessage = await taskPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
-    expect(validationMessage).toEqual('This is a required field')
-})
+    const validationMessage = await taskPage.inputTaskName.evaluate(
+      (e) => (e as HTMLInputElement).validationMessage
+    );
+    expect(validationMessage).toEqual("This is a required field");
+  });
+});
+
+test.describe("atualização", () => {
+  test("Deve concluir uma tarefa", async ({ page, request }) => {
+    const task = data.update as TaskModel;
+
+    await deleteTaskByHelper(request, task.name);
+    await postTask(request, task);
+
+    const tasksPage: TasksPage = new TasksPage(page);
+
+    await tasksPage.go();
+    await tasksPage.toogle(task.name);
+    await tasksPage.shouldBeDone(task.name);
+  });
+});
